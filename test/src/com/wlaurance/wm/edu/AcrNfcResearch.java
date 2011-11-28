254dac0e8b985f6d14c0ad9cdd5bf6b5ae9266c8
wlaurance <w.laurance@gmail.com>
2011-11-27 21:10:25 -0500

    Print terminals + if card is present

--- test/src/com/wlaurance/wm/edu/AcrNfcResearch.java
+++ test/src/com/wlaurance/wm/edu/AcrNfcResearch.java
@@ -6,16 +6,41 @@ import javax.smartcardio.CardException;
 import javax.smartcardio.CardTerminal;
 import javax.smartcardio.TerminalFactory;
 
+import ds.nfcip.se.NFCIPConnection;
+
 public class AcrNfcResearch {
-	public static void main(String args[]){
-		List<CardTerminal> terminals;
+	private NFCIPConnection connection = null;
+
+	public static void main(String args[]) {
+		List<CardTerminal> terminals = null;
 		TerminalFactory fac = TerminalFactory.getDefault();
 		try {
 			terminals = fac.terminals().list();
 			System.out.println(terminals);
 		} catch (CardException e) {
-			// TODO Auto-generated catch block
+			System.err.println("No terminals detected");
 			e.printStackTrace();
+		}
+		printTerminalStatus(terminals);
+	}
+
+	public void initConnection() {
+		connection = new NFCIPConnection();
+	}
+
+	public static void printTerminalStatus(List<CardTerminal> t) {
+		if (t != null) {
+			for (CardTerminal terminal : t) {
+				try {
+					System.out.println(terminal.toString() + "  "
+							+ terminal.isCardPresent());
+				} catch (CardException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		} else {
+			System.err.println("CardTerminal list is null");
 		}
 	}
 }
