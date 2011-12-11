3fae7c89e33f20c4a012e97d3175014bbba56ad8
wlaurance <w.laurance@gmail.com>
2011-12-10 23:09:51 -0500

    I can read/write a url to the card and read it.

--- test/src/com/wlaurance/wm/edu/Mifare1kTagConnection.java
+++ test/src/com/wlaurance/wm/edu/Mifare1kTagConnection.java
@@ -4,29 +4,32 @@ import java.util.ArrayList;
 
 import ds.nfcip.se.NFCIPConnection;
 
-public class Mifare1kTagConnection extends TagConnection{
+public class Mifare1kTagConnection extends TagConnection {
 	ACRNfcConnection connection;
-	static final Integer MIFARE1K_BLOCK_SIZE = 16; 
-	static final int BLOCK_START = 1; 
+	static final Integer MIFARE1K_BLOCK_SIZE = 16;
+	static final int BLOCK_START = 1;
 	static final int MIFARE1K_SECTOR_LENGTH = 16;
 	static final int MIFARE1K_BLOCK_PER_SECTOR = 4;
 	static final int MIFARE1K_AVAILABLE = 752;
-	public Mifare1kTagConnection(){
+
+	public Mifare1kTagConnection() {
 		connection = new ACRNfcConnection(false);
 	}
-	
-	public void writeToCard(String data) throws TagConnectionException{
+
+	public void writeToCard(String data) throws TagConnectionException {
 		byte[] bytes = data.getBytes();
-		if (ACRNfcConnection.DEBUG) NFCIPConnection.printByteArray(bytes);
+		if (ACRNfcConnection.DEBUG)
+			NFCIPConnection.printByteArray(bytes);
 		int length = bytes.length;
-		if (length > MIFARE1K_AVAILABLE){
-			throw new TagConnectionException("Cannot write this much data to the card");
+		if (length > MIFARE1K_AVAILABLE) {
+			throw new TagConnectionException(
+					"Cannot write this much data to the card");
 		}
 		byte[] bytePointer = null;
 		ArrayList<byte[]> myList = new ArrayList<byte[]>();
-		for (int i = 0; i < length; i++){
-			int relativeByte = i % MIFARE1K_BLOCK_SIZE; 
-			if (relativeByte == 0){
+		for (int i = 0; i < length; i++) {
+			int relativeByte = i % MIFARE1K_BLOCK_SIZE;
+			if (relativeByte == 0) {
 				if (bytePointer != null)
 					myList.add(bytePointer);
 				bytePointer = new byte[MIFARE1K_BLOCK_SIZE];
@@ -34,29 +37,34 @@ public class Mifare1kTagConnection extends TagConnection{
 			} else {
 				bytePointer[relativeByte] = bytes[i];
 			}
-			
-			if (i+1 >= length && bytePointer != null){
+
+			if (i + 1 >= length && bytePointer != null) {
 				myList.add(bytePointer);
 			}
 		}
-		
+
 		Integer block = BLOCK_START;
-		for (byte[] x : myList){
+		for (byte[] x : myList) {
 			connection.writeCardBinary(block.byteValue(), x);
 			block++;
 		}
-		
+
 	}
 
 	@Override
 	public String readFromCard() {
 		ArrayList<byte[]> bytes = new ArrayList<byte[]>();
-		for (Integer i = BLOCK_START; i < (MIFARE1K_SECTOR_LENGTH * MIFARE1K_BLOCK_PER_SECTOR); i++ ){
-			bytes.add(connection.readCardBinary(i.byteValue(), MIFARE1K_BLOCK_SIZE.byteValue()));
+		for (Integer i = BLOCK_START; i < (MIFARE1K_SECTOR_LENGTH); i++) {
+			bytes.add(connection.readCardBinary(i.byteValue(),
+					MIFARE1K_BLOCK_SIZE.byteValue()));
 		}
-		String message = "";
-		for (byte[] x : bytes)
-			message = message + new String(x);
-		return message;
+		
+		String one = "";
+		for (byte[] x : bytes){
+			String temp = new String(x);
+			one = one.concat(temp.substring(0, temp.length()-2));
+		}
+		
+		return one;
 	}
 }
